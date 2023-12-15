bff7f653917b1cb91c4546a9b979f349bb29f0fb
Sven Rademakers <sven.rademakers@gmail.com>
2023-12-15 10:29:46 +0000

    commit cargo skeleton

---
+++ src/lib.rs
@@ -0,0 +1,14 @@
+use wasm_bindgen::prelude::*;
+
+#[global_allocator]
+static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+
+#[wasm_bindgen]
+extern {
+    pub fn alert(s: &str);
+}
+
+#[wasm_bindgen]
+pub fn greet(name: &str) {
+    alert(&format!("Hello, {}!", name));
+}
